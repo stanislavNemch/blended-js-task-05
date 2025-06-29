a82f564d2ea64ebf615fe5a9f1b09dd7f8b01430
Stanislav Nemchenko <stas.buka@gmail.com>
2025-06-29 03:54:02 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -50,6 +50,10 @@ export default defineConfig(({ command }) => {
           isBuild &&
             purgecss.default({
               content: ['./src/**/*.html', './src/js/**/*.js'],
+              safelist: {
+                standard: ['is-active', 'is-visible'],
+                deep: [/swiper/, /modal/],
+              },
             }),
         ].filter(Boolean),
       },
